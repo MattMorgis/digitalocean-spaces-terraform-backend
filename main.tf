102c8a25a7fb7a0dcad18dcf7d8a395990262f32
MattMorgis <matt@rappiddevelopment.com>
2019-03-22 19:47:59 -0400

    inital commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "s3" {
+    endpoint                    = "sfo2.digitaloceanspaces.com"
+    region                      = "us-west-1"
+    key                         = "terraform.tfstate"
+    bucket                      = "rappiddev-terraform-remote-state"
+    skip_requesting_account_id  = true
+    skip_credentials_validation = true
+    skip_get_ec2_platforms      = true
+    skip_metadata_api_check     = true
+  }
+}
+
+provider "digitalocean" {
+  # export DIGITALOCEAN_TOKEN="API TOKEN"
+}
+
+resource "digitalocean_droplet" "app" {
+  name   = "my-first-tf-droplet"
+  image  = "ubuntu-18-04-x64"
+  region = "nyc1"
+  size   = "512mb"
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "do_nyc1" {
+  description = "Digital Ocean New York Data Center 1"
+  default     = "nyc1"
+}
+
+variable "do_nyc2" {
+  description = "Digital Ocean New York Data Center 2"
+  default     = "nyc2"
+}
+
+variable "do_nyc3" {
+  description = "Digital Ocean New York Data Center 3"
+  default     = "nyc3"
+}
+
+variable "ubuntu" {
+  description = "Default LTS"
+  default     = "ubuntu-16-04-x64"
+}
